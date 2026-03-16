40df3a62d4e3cf17bf821a0ed258d935239c8810
佐藤希帆 <>
2026-03-17 07:14:56 +0900

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Button from './components/Button/Button.jsx'
 
 function App() {
 
+  const handleClick = () => {
+    alert("Thanksss...");
+  }
+
   return (
     <>
       {/* ↑の「空のフラグメント」について、returnの中に複数行書きたい場合は何かしらのタグで囲む必要があり。今回は「空のフラグメント」だが、divとかでもOK */}
       <h1>Hello World</h1>
-      <Button />
+      {/* <Button type="submit" disabled={true} /> */}
+      <Button type="submit" disabled={false} onClick={handleClick}>
+        Click here!{/* ←children */}
+      </Button>
     </>
   )
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,17 +5,26 @@
 // }
 import styles from "./Button.module.css";
 
-function Button() {
+function Button(props) {
+
+  // Propsの受け取り
+  const { type, disabled, children, onClick } = props;
 
   // 必要なロジックは関数とリターンの間に書く
-  const handleClick = () => {
-    alert("It's licked");
-  }
+  // const handleClick = () => {
+  //   alert("It's licked");
+  // }
+
+  // return (
+  //   <button className={styles.button} type='button' onClick={handleClick}>
+  //     <i>icon</i>
+  //     <span>click</span>
+  //   </button>
+  // )
 
   return (
-    <button className={styles.button} type='button' onClick={handleClick}>
-      <i>icon</i>
-      <span>click</span>
+    <button type={type} disabled={disabled} onClick={onClick}>
+      {children}
     </button>
   )
 }
